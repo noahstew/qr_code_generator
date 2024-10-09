a4e7badd472e967141cfd4845f9f0b68880cf26c
Noah Stewart <njstewart22@icloud.com>
2024-10-08 22:08:08 -0700

    Last try of night

--- app/root.jsx
+++ app/root.jsx
@@ -13,7 +13,7 @@ export default function App() {
         <Meta />
         <Links />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
